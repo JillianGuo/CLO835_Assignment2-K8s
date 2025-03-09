0e80014de050985f08ae450682f66f569ada51df
J.Guo <jiyun.guo@gmail.com>
2025-03-09 06:27:33 -0400

    Update config.tf

--- EC2/deployment/config.tf
+++ EC2/deployment/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "clo835-assignment2-dev-s3-bucket"    // Bucket where to SAVE Terraform State
-    key    = "terraform.tfstate"                   // Object name in the bucket to SAVE Terraform State
+    key    = "terraform/terraform.tfstate"         // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"                           // Region where bucket is created
   }
 }
